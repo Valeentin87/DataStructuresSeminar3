9c46f73928d5f2cc5127f18382992f63cb9d04f2
Valeentin87 <valeentin87@list.ru>
2023-10-18 14:50:27 +0300

    реализован класс узла с возможностью окрашивания в цвет

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,27 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
+
+
+
+    class Node {
+
+        Node left, right;
+        int data;
+
+        // красный ==> true, черный ==> false
+        boolean color;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+
+// Новый узел, который создается, является всегда красного цвета.
+            color = true;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
